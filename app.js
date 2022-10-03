8279af0382e9d7cd603cb103b8e452894556c6da
HilalVildan <hilalsarioglu96@gmail.com>
2022-10-03 14:28:28 +0200

    update

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
-//* =================================== Checkout Page Solution
-//* map filter, dest,spread=======================================================================
+//* ======================================================================
+//*                 Checkout Page Solution
+//*  map filter, dest,spread==============================================
+//!table da kullanılacak değişkenler
+const kargo = 15.0;
+const vergi = 0.18;
 
 let sepettekiler = [
-  { name: "Vintage Backpack", price: 34.99, adet: 1, img: "/img/photo1.png" },
-  { name: "Levi Shoes", price: 40.99, adet: 1, img: "/img/photo2.png" },
+  { name: "Vintage Backpack", price: 34.99, adet: 1, img: "./img/photo1.png" },
+  { name: "Levi Shoes", price: 40.99, adet: 1, img: "./img/photo2.png" },
   { name: "Antique Clock", price: 69.99, adet: 1, img: "./img/photo3.jpg" },
 ];
 
-//!ekrana bastirma
+//!EKRANA BASTIRMA
 sepettekiler.forEach((ürün) => {
-  //!DESTRUCTRING
+  //!DESTRUCTURİNG
   const { name, price, adet, img } = ürün;
 
   document.querySelector(
-    "#ürün-rowlari"
+    "#urun-rowlari"
   ).innerHTML += `<div class="card mb-3" style="max-width: 540px;">
 
   <div class="row g-0">
 
     <div class="col-md-5">
-      <img src=${img} class="img-fluid rounded-start" alt="...">
+      <img src= ${img}  class="img-fluid rounded-start" alt="...">
     </div>
 
     <div class="col-md-7">
@@ -30,10 +34,10 @@ sepettekiler.forEach((ürün) => {
         
              <div class="ürün-price">
                     <p class="text-warning h2">$
-                      <span class="indirim-price"> ${(price * 0.7).toFixed(
+                      <span class="indirim-price">${(price * 0.7).toFixed(
                         2
                       )}</span>
-                      <span class="h5 text-dark text-decoration-line-through">${price}</span>
+                      <span class="h5 text-dark text-decoration-line-through">${price} </span>
                     </p>
                   </div>
 
@@ -42,7 +46,7 @@ sepettekiler.forEach((ürün) => {
                     class="border border-1 border-dark shadow-lg d-flex justify-content-center p-2"
                   >
                     <div class="adet-controller">
-                      <button class="btn btn-secondary btn-sm">
+                      <button class="btn btn-secondary btn-sm minus">
                         <i class="fas fa-minus"></i>
                       </button>
                       <p class="d-inline mx-4" id="ürün-adet">${adet}</p>
@@ -60,11 +64,11 @@ sepettekiler.forEach((ürün) => {
                   </div>
 
                   <div class="mt-2">
-                    Ürün Toplam:$<span class="ürün-toplam">${(
+                    Ürün Toplam: $<span class="ürün-toplam">${(
                       price *
                       0.7 *
                       adet
-                    ).toFixed(2)} </span>
+                    ).toFixed(2)}</span>
                   </div>
       </div>
     </div>
@@ -72,18 +76,18 @@ sepettekiler.forEach((ürün) => {
 </div>`;
 });
 
-//!SILME
-
+//!SİLME
 document.querySelectorAll(".remove-ürün").forEach((btn) => {
   btn.onclick = () => {
     removeSil(btn);
   };
 });
-//!ekrandan sildik
-function removeSil(btn) {
-  //btn.parentElement.parentElement.parentElement.parentElement.parentElement.remove();
 
-  //? 2. yol closest() = istediginiz class isimli parentElement e kadar cikar
+function removeSil(btn) {
+  //!ekrandan sildik
+  //* btn.parentElement.parentElement.parentElement.parentElement.parentElement.remove()
+
+  //?2. yol closest()= istediğiniz class isimli parentElement e kadar çıkar
   btn.closest(".card").remove();
 
   //!diziden sildik
@@ -95,16 +99,37 @@ function removeSil(btn) {
   console.log(sepettekiler);
 }
 
-//!ADET DEGISTIRME
+//!ADET DEĞİŞTİRME
 
 adetButon();
 
-function adetButon() {
-  //!burada - adet ve + elementlerle işim olduğu için, mesela - ye basınca adet (kardeşi) değişsin
-  //!istediğim için, minus a ulaşıp ona tıklanınca closest ile parent ına oradan da kardeşine ulaş
-  //!eksilt diyebiliriz. ya da gerekli elementlerin parent ına ulaşıp çocuklarına adlar verip, artık o
-  //!adlarla işlem yapabiliriz
+//!todo browser da en alttaki total kısmı
+document.querySelector("#odeme-table").innerHTML = `<table class="table">
+            <tbody>
+              <tr class="text-end">
+                <th class="text-start">Aratoplam</th>
+                <td>$<span class="aratoplam">0.00</span></td>
+              </tr>
+              <tr class="text-end">
+                <th class="text-start">Vergi(18%)</th>
+                <td>$<span class="vergi">0.00</span></td>
+              </tr>
+              <tr class="text-end">
+                <th class="text-start">Kargo</th>
+                <td>$<span class="kargo">0.00</span></td>
+              </tr>
+              <tr class="text-end">
+                <th class="text-start">Toplam</th>
+                <td>$<span class="toplam">0.00</span></td>
+              </tr>
+            </tbody>
+          </table>`;
 
+//******************************** */
+hesaplaCardTotal();
+
+function adetButon() {
+  //!burada - adet ve + elementlerle işim olduğu için, mesela - ye basınca adet (kardeşi) değişsin istediğim için, minus a ulaşıp ona tıklanınca closest ile parent ına oradan da kardeşine ulaş eksilt diyebiliriz. ya da gerekli elementlerin parent ına ulaşıp çocuklarına adlar verip, artık o adlarla işlem yapabiliriz
   document.querySelectorAll(".adet-controller").forEach((kutu) => {
     const minus = kutu.firstElementChild;
     const adet1 = kutu.querySelector("#ürün-adet");
@@ -113,29 +138,75 @@ function adetButon() {
       //!minus adet değişimini ekrana bastır
       adet1.textContent = adet1.textContent - 1;
 
-      //!sepettekilerde adet degisimi yapalim
+      //!sepettekiler de adet değişimini yapalım
 
       sepettekiler.map((ürün) => {
         if (
           ürün.name == adet1.closest(".card").querySelector("h5").textContent
         ) {
           ürün.adet = Number(adet1.textContent);
-        } //number yerine sadece + isareti de koysan stringi numbera cevirir.
+        }
       });
       console.log(sepettekiler);
-
       //!ürün toplam ı ekrana bastırma (her ürün card ında var)
       adet1.closest(".row").querySelector(".ürün-toplam").textContent =
         adet1.closest(".row").querySelector(".indirim-price").textContent *
         adet1.textContent;
+      //?????????????????????????????
+      hesaplaCardTotal();
 
-      //!eger adet 1 iken tekrar minusa basilirsa o ürünü sil. (minus butonu removeSil(btn) foks gitsin,
-      //!parentini silsin
-
+      //!eğer adet 1 iken tekrar minus a basılırsa o ürünü sil (minus butonu removeSil fonksiyonuna gitsin parent ını silsin)
       if (adet1.textContent < 1) {
-        alert("sileyim mi?");
+        alert("sileyim mi");
         removeSil(minus);
       }
     };
+
+    //! plus a basınca minus a benzer işlemler
+    const plus = kutu.lastElementChild;
+    plus.onclick = () => {
+      adet1.textContent = Number(adet1.textContent) + 1;
+      //????????????????????????????????????BU KISIM HEM MİNUS HEM PLUS TA VAR FONKSİYON A ATILABİLİR
+      //!diziyi güncelle
+      sepettekiler.map((ürün) => {
+        if (
+          ürün.name ==
+          adet1.closest(".row").querySelector(".card-title").textContent
+        )
+          ürün.adet = Number(adet1.textContent);
+        console.log(sepettekiler);
+      });
+      //!ürün toplam ekrana bastırması. her üründe olan toplam kısmı
+      adet1.closest(".row").querySelector(".ürün-toplam").textContent = (
+        adet1.closest(".row").querySelector(".indirim-price").textContent *
+        adet1.textContent
+      ).toFixed(2);
+      hesaplaCardTotal();
+    };
   });
+}
+//! Calculate and update card total values
+function hesaplaCardTotal() {
+  //! her bir card daki ürün toplam kısımları
+  const ürünToplam = document.querySelectorAll(".ürün-toplam");
+
+  //!  Bir NodeListnesne, bir belgeden çıkarılan düğümlerin bir listesidir
+
+  //? araToplam= en alttaki tüm ürünler için vergi kargo hariç sepettekiler fiyatı
+  //?Reduce tam olarak Array istiyor, nodelist yeterli değil
+
+  //*önce hesapla sonra altta browser a (DOM) bastır
+  //  console.log([...ürünToplam]);
+  const araToplam = Array.from(ürünToplam).reduce(
+    (acc, item) => acc + Number(item.textContent),
+    0
+  );
+  const vergiPrice = araToplam * vergi;
+  const shipping = araToplam > 0 ? kargo : 0;
+  const cardTotal = araToplam + shipping + vergiPrice;
+
+  document.querySelector(".aratoplam").textContent = araToplam.toFixed(2);
+  document.querySelector(".vergi").textContent = vergiPrice.toFixed(2);
+  document.querySelector(".kargo").textContent = shipping.toFixed(2);
+  document.querySelector(".toplam").textContent = cardTotal.toFixed(2);
 }
